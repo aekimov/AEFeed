e6e8d51500ce56b07ac33810924ab126aa03d41e
Artem Ekimov <tehman3d@mail.ru>
2022-10-29 20:09:54 +0400

    Initial Commit

---
+++ AEFeed/AEFeed.h
@@ -0,0 +1,18 @@
+//
+//  AEFeed.h
+//  AEFeed
+//
+//  Created by Artem Ekimov on 10/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AEFeed.
+FOUNDATION_EXPORT double AEFeedVersionNumber;
+
+//! Project version string for AEFeed.
+FOUNDATION_EXPORT const unsigned char AEFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AEFeed/PublicHeader.h>
+
+
