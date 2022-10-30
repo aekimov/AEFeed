b7b9867ce8484dd36aea197dcc4c8dfda1b90a6f
Artem Ekimov <tehman3d@mail.ru>
2022-10-30 08:29:49 +0400

    Create MoviesFeed and FeedItem models.

---
+++ AEFeed/FeedItem.swift
@@ -0,0 +1,32 @@
+//
+//  FeedItem.swift
+//  AEFeed
+//
+//  Created by Artem Ekimov on 10/29/22.
+//
+
+import Foundation
+
+public struct FeedItem: Equatable {
+    public let id: Int
+    public let title: String
+    public let imagePath: String
+    
+    public init(id: Int, title: String, imagePath: String) {
+        self.id = id
+        self.title = title
+        self.imagePath = imagePath
+    }
+}
+
+public struct MoviesFeed: Equatable {
+    public let items: [FeedItem]
+    public let page: Int
+    public let totalPages: Int
+    
+    public init(items: [FeedItem], page: Int, totalPages: Int) {
+        self.items = items
+        self.page = page
+        self.totalPages = totalPages
+    }
+}
