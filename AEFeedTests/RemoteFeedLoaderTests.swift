2cb523657edf0e65aacbab794dd826e4d54cab70
Artem Ekimov <tehman3d@mail.ru>
2022-10-30 09:43:31 +0400

    Add makeSUT factory method, add HTTPClient protocol.

--- AEFeedTests/RemoteFeedLoaderTests.swift
+++ AEFeedTests/RemoteFeedLoaderTests.swift
@@ -6,20 +6,41 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    
-}
-
-class HTTPClient {
-    var requestedURL: URL?
-}
+import AEFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestData() {
-        let client = HTTPClient()
-        let _ = RemoteFeedLoader()
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let (sut, client) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
+    //MARK:- Helpers
+    
+    private func makeSUT() -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
