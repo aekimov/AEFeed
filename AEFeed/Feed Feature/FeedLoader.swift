382ce4a4049562803f23e5ae536ce1dec4b276f4
Artem Ekimov <tehman3d@mail.ru>
2022-10-30 08:38:58 +0400

    Create FeedLoader protocol, group files.

---
+++ AEFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoader.swift
+//  AEFeed
+//
+//  Created by Artem Ekimov on 10/30/22.
+//
+
+import Foundation
+
+public protocol FeedLoader {
+    typealias Result = Swift.Result<MoviesFeed, Error>
+    func load(_ req: URLRequest, completion: @escaping (Result) -> Void)
+}
