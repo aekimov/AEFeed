6ef5078553da6d2f33d487321676e9eeb531b6fd
Artem Ekimov <tehman3d@mail.ru>
2022-11-01 08:38:12 +0400

    Request delete on save, create makeSUT factory.

--- AEFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ AEFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,13 +6,18 @@
 //
 
 import XCTest
-
+import AEFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
     }
     
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeedCallCount += 1
+    }
 }
 
 class FeedStore {
@@ -21,8 +26,31 @@ class FeedStore {
 
 class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID().hashValue, title: "any", imagePath: "/any-path")
+    }
+
 }
