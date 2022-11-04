108885e679bb9d0c4237881de91cceb3452bdc4e
Artem Ekimov <tehman3d@mail.ru>
2022-11-04 11:57:58 +0400

    Move FeedStore to a separate file.

---
+++ AEFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  AEFeed
+//
+//  Created by Artem Ekimov on 11/4/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
