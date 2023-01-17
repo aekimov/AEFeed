db7743fc9ca7efabefafb0c8e9cf723361906bc2
Artem Ekimov <tehman3d@mail.ru>
2023-01-17 19:51:46 +0400

    Create CoreData stack. Make storeURL an explicit dependency so we can inject test-specific URLs

--- AEFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ AEFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -10,15 +10,19 @@ import AEFeed
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        
+
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
@@ -61,7 +65,9 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-        let sut = CoreDataFeedStore()
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
