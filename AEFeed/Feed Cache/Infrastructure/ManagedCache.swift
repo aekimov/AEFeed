e7db2eb80f5b6853697aabc9076c54891f863e65
Artem Ekimov <tehman3d@mail.ru>
2023-01-18 21:13:13 +0400

    Integration of LocalFeedLoader and CoreDataFeedStore delivers no items on empty cache

---
+++ AEFeed/Feed Cache/Infrastructure/ManagedCache.swift
@@ -0,0 +1,31 @@
+//
+//  ManagedCache.swift
+//  AEFeed
+//
+//  Created by Artem Ekimov on 1/18/23.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    static func findCache(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try findCache(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    
+    var localFeed: [LocalFeedImage] {
+        return feed
+            .compactMap { $0 as? ManagedFeedImage }
+            .map { LocalFeedImage(id: $0.id.intValue, title: $0.title, imagePath: $0.imagePath) }
+    }
+}
---
+++ AEFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
@@ -0,0 +1,26 @@
+//
+//  ManagedFeedImage.swift
+//  AEFeed
+//
+//  Created by Artem Ekimov on 1/18/23.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: NSNumber
+    @NSManaged var title: String
+    @NSManaged var imagePath: String
+    @NSManaged var cache: ManagedCache
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map {
+            let managed = ManagedFeedImage(context: context)
+            managed.id = NSNumber(value: $0.id)
+            managed.title = $0.title
+            managed.imagePath = $0.imagePath
+            return managed
+        })
+    }
+}
