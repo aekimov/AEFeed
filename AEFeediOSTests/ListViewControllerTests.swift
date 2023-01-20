1a1af9f989d8680cd7417a085efb0e951c1d241f
Artem Ekimov <tehman3d@mail.ru>
2023-01-20 19:08:56 +0400

    Show and hide loading indicator on pull to refresh

--- AEFeediOSTests/ListViewControllerTests.swift
+++ AEFeediOSTests/ListViewControllerTests.swift
@@ -73,7 +73,23 @@ class ListViewControllerTests: XCTestCase {
         
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
+    
+    func test_pullToRefresh_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
 
+    func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
     
     
     //MARK: - Helpers
