13ee80c09c0b6887a391d80f8dcd586cfc61b2ce
Artem Ekimov <tehman3d@mail.ru>
2023-01-21 10:06:33 +0400

    Extract adapter pattern into a separate function

--- AEFeediOS/FeedUIComposer.swift
+++ AEFeediOS/FeedUIComposer.swift
@@ -15,11 +15,15 @@ public class FeedUIComposer {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let viewController = ListViewController(refreshController: refreshController)
         
-        refreshController.onRefresh = { [weak viewController] feed in
-            viewController?.models = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: imageLoader)
+        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: viewController, loader: imageLoader)
+        return viewController
+    }
+    
+    private static func adaptFeedToCellControllers(forwardingTo controller: ListViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak controller] feed in
+            controller?.models = feed.map { model in
+                FeedImageCellController(model: model, imageLoader: loader)
             }
         }
-        return viewController
     }
 }
