181d3dd7909e2bc5137fca42e04ca9dbb4501113
Artem Ekimov <tehman3d@mail.ru>
2023-02-01 16:36:42 +0400

    Add UI Test target

---
+++ AEFeedApp/AEFeedAppUIAcceptanceTests/AEFeedAppUIAcceptanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  AEFeedAppUIAcceptanceTests.swift
+//  AEFeedAppUIAcceptanceTests
+//
+//  Created by Artem Ekimov on 2/1/23.
+//
+
+import XCTest
+
+final class AEFeedAppUIAcceptanceTests: XCTestCase {
+    
+    func test() {
+        XCTFail()
+    }
+    
+}
